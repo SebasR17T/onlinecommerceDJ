666e46dea9b0035d995a678974a1287d8abfedfd
Tu Nombre <Sebastiancdlm1101@gmail.com>
2024-09-19 10:53:32 -0500

    Last update

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -14,3 +14,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
+
+import sys
+path = '/home/tu_usuario/onlinecommercePY'
+if path not in sys.path:
+    sys.path.append(path)
+
+from django.core.wsgi import get_wsgi_application
+os.environ['DJANGO_SETTINGS_MODULE'] = 'onlinecommerce.settings'
+application = get_wsgi_application()
